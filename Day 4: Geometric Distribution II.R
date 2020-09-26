cf379bd8fcb5d496654e03dc61ed25d28783d8be
chongchong6 <55506640+chongchong6@users.noreply.github.com>
2020-09-26 00:00:52 -0700

    Update Day 4: Geometric Distribution II.R

--- Day 4: Geometric Distribution II.R
+++ Day 4: Geometric Distribution II.R
@@ -1,4 +1,10 @@
 p <- 1/3
+result <- sum(dgeom(0:4, p))
+
+cat(round(result,3))
+
+--------------------------------------------------
+p <- 1/3
 
 result <- 1 - dbinom(0, 5, p)
 #result <- sum(dbinom(1:5, 5, p))
