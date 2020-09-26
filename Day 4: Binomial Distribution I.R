59dc3ba5cf41e966a361f9eaeae7ddebfd3016a6
chongchong6 <55506640+chongchong6@users.noreply.github.com>
2020-09-25 23:14:34 -0700

    Update Day 4: Binomial Distribution I.R

--- Day 4: Binomial Distribution I.R
+++ Day 4: Binomial Distribution I.R
@@ -1,24 +1,12 @@
 # the probability of boy
-#p <- 1.09/(1.09 + 1)
+p <- 1.09/(1.09 + 1)
 
 # dbinom(x, size, prob)
 # x is a vector of numbers.
 # size is the number of trials.
 # prob is the probability of success of each trial.
 
-result <- dbinom(6, 6, p) - dbinom(2, 6, p)
-
-cat(round(result, 3))
-
---------------------------------------------------------------------------
-
-f <- file("stdin")
-T <- lapply(strsplit(readLines(f), " "), as.numeric)
-
-b <- T[[1]][1]
-g <- T[[1]][2]
-
-pb <- b/(b+g)
-result <- sum(dbinom(3:6,size =6, prob =p))
+result <- 1 - dbinom(2, 6, p) - dbinom(1, 6, p) - dbinom(0, 6, p)
+# result <- sum(dbinom(3:6,size =6, prob =p))
 
 cat(round(result, 3))
