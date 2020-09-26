cc52f952ceeaa1431562802b4e4d1ece4e308789
chongchong6 <55506640+chongchong6@users.noreply.github.com>
2020-09-25 23:46:59 -0700

    Update Day 4: Geometric Distribution I.R

--- Day 4: Geometric Distribution I.R
+++ Day 4: Geometric Distribution I.R
@@ -5,3 +5,12 @@ p <- 1/3
 result <- p * (1-p)^4
 
 cat(round(result,3))
+
+
+-----------------------------------------
+#R function dgeom(x, prob) is the probability of x failures prior to the first success (note the difference) when the probability of success is prob.
+
+p <- 1/3
+x <- 5 - 1
+result <- dgeom(x, p)
+cat(round(result,3))
